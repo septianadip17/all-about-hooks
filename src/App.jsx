97df7823a7c35917be279d94b16fd3971a8484cd
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-06 06:54:15 +0700

    use state

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ import State from "./components/State";
 
 const App = () => {
   return (
-    <div className=" justify-center items-center grid space-y-5 mt-20 ">
-      <h1 className=" text-purple-500 text-5xl font-bold ">
+    <div className="container justify-center items-center space-y-5 mt-20 mx-auto w-full h-screen">
+      <h1 className="text-center text-purple-500 text-5xl font-bold ">
         Hello, this is all about hooks
       </h1>
       <hr className=" border-purple-500" />
-      <div className=" grid grid-cols-3 gap-5">
+      <div className="text-white text-xl grid grid-rows-3 gap-5 justify-between">
         <State />
         <Effect />
         <Ref />
--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 const State = () => {
-  return (
-    <div>State</div>
-  )
-}
+  const [color, setColor] = useState("Red");
+  const changeColor = () => {
+    setColor("Blue");
+  };
 
-export default State
+  return (
+    <div className="border-4 border-yellow-400">
+      <h1 className="text-3xl">State</h1>
+      <h1>My Favourite color is {color}</h1>
+      <button onClick={changeColor}>Change Color</button>
+    </div>
+  );
+};
+
+export default State;
