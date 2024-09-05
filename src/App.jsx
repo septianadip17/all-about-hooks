9f70e4977cb506001d97d3ed83768b7329a1848c
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-06 05:49:43 +0700

    custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import Callback from "./components/Callback";
+import Context from "./components/Context";
+import Custom from "./components/Custom";
+import Effect from "./components/Effect";
+import LayoutEffect from "./components/LayoutEffect";
+import Memo from "./components/Memo";
+import Reducer from "./components/Reducer";
+import Ref from "./components/Ref";
 import State from "./components/State";
 
 const App = () => {
@@ -6,10 +14,17 @@ const App = () => {
       <h1 className=" text-purple-500 text-5xl font-bold ">
         Hello, this is all about hooks
       </h1>
-      <hr className=" border-purple-500"/>
+      <hr className=" border-purple-500" />
       <div className=" grid grid-cols-3 gap-5">
-
-      <State />
+        <State />
+        <Effect />
+        <Ref />
+        <Memo />
+        <Callback />
+        <Context />
+        <Reducer />
+        <LayoutEffect />
+        <Custom />
       </div>
     </div>
   );
--- src/components/CustomHook.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CustomHook = () => {
-  return (
-    <div>CustomHook</div>
-  )
-}
-
-export default CustomHook
