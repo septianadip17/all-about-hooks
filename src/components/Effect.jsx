dd0c1a3f7a504207c96c5575059e489f15e96245
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-06 18:42:39 +0700

    use effect added method

--- src/components/Effect.jsx
+++ src/components/Effect.jsx
@@ -1,21 +1,47 @@
 import React, { useEffect, useState } from "react";
+import Buttons from "./Buttons";
 
 const Effect = () => {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("GreatStack");
 
-  // 
-  useEffect(() =>{
-    setTimeout(() =>{
-      setCount(count => count + 1);
-    }, 2000)
-  }, [count, name])
+  // pacar
+  const [pacarSaya, setPacarSaya] = useState(1);
+
+  // counter
+  useEffect(() => {
+    setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 2000);
+  }, [count, name]);
+
+  // alert hehe
+  useEffect(() => {
+    alert("Halo hehee");
+  }, []);
 
   return (
-    <div className="border-4 border-yellow-400 w-screen mx-auto container">
+    <div className="border-4 border-yellow-400 w-screen mx-auto container p-5">
       <h1 className="text-3xl">useEffect</h1>
+
+      {/*  */}
       <div className="flex gap-2 items-center justify-center m-2">
         <h1>I've rendered {count} times</h1>
+      </div>
+
+      <hr className="border-2 border-green-500" />
+
+      {/* Pacar */}
+      <div className="flex gap-2 items-center justify-center m-2">
+        <h1>saya memiliki: {pacarSaya} pacar</h1>
+        <Buttons
+          onClick={() => setPacarSaya((prev) => prev + 1)}
+          name="tambah pacar"
+        />
+        <Buttons
+          onClick={() => setPacarSaya((prev) => prev - 1)}
+          name="putuskan pacar"
+        />
       </div>
     </div>
   );
