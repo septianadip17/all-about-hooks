214ba8ddd73ffc9105bfc0fabc9b655b29b724fa
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-06 15:50:37 +0700

    eslint react/no unescaped entities off and useEffect

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,5 +18,6 @@ module.exports = {
       { allowConstantExport: true },
     ],
     "no-unused-vars": "off",
+    "react/no-unescaped-entities": "off",
   },
 };
--- src/components/Effect.jsx
+++ src/components/Effect.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Effect = () => {
-  return <div className='border-4 border-yellow-400 w-screen mx-auto container'>Effect</div>;
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("GreatStack");
+
+  useEffect(() =>{
+    setTimeout(() =>{
+      setCount(count => count + 1);
+    }, 2000)
+  }, [count, name])
+
+  return (
+    <div className="border-4 border-yellow-400 w-screen mx-auto container">
+      <h1 className="text-3xl">useEffect</h1>
+      <div className="flex gap-2 items-center justify-center m-2">
+        <h1>I've rendered {count} times</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Effect;
