a0a08462c16b1bc38189153fc8211eb30711d894
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-08 16:31:51 +0700

    header for usecallback

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+  console.log("Header rendered");
+  return <div className="border-4 border-yellow-400 w-screen mx-auto container">Header</div>;
+};
+
+export default Header;
