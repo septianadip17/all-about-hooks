01594752307521c3284715c3641bb921f2a8db37
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-08 13:18:04 +0700

    input name

--- src/components/Ref.jsx
+++ src/components/Ref.jsx
@@ -1,13 +1,32 @@
 import React, { useEffect, useState, useRef } from "react";
 
 const Ref = () => {
+  const [name, setName] = useState("");
+  const renderCount = useRef(1);
+
+  useEffect(() => {
+    renderCount.current += 1;
+  });
+
   return (
-    <div className="border-4 border-red-400 w-screen mx-auto container p-5">
-      <h1 className="text-3xl">useRef</h1>
-      <div className='className="flex gap-2 text-center items-center justify-center m-2'>
-        <h1>This is useRef</h1>
+    <div className="border-4 border-red-400  mx-auto p-5">
+      <h1 className="text-3xl text-start">useRef</h1>
+      <div className="flex flex-col gap-2 items-center justify-center m-2">
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter your name"
+          className="w-2/3 border rounded p-2 text-black"
+        />
+        <h2 className="text-xl">My name is {name}</h2>
+        <h2 className="text-xl">I rendered {renderCount.current} times</h2>
       </div>
-      <hr className="border-2 border-green-500" />
+      <hr className="border-2 border-green-500 my-4" />
+      <div className="text-center">
+        <h2>This is useRef</h2>
+      </div>
+      <hr className="border-2 border-green-500 my-4" />
       <div className="flex gap-2 text-center items-center justify-center m-2">
         <p className="w-2/3 text-justify">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. A sequi
@@ -21,10 +40,10 @@ const Ref = () => {
           obcaecati, praesentium dolorem fugiat exercitationem.
         </p>
       </div>
-      <div className="flex items-center">
+      <div className="flex justify-center mt-4">
         <a
           href="https://reactjs.org"
-          className="border-2 border-blue-600 mx-auto"
+          className="border-2 border-blue-600 p-2 rounded hover:bg-blue-600 hover:text-white transition duration-300"
           target="_blank"
           rel="noopener noreferrer"
         >
