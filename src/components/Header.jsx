b615692fe3a210ac34fb35acd470f580ec2f2872
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-08 16:47:52 +0700

    text xl

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Header = () => {
   console.log("Header rendered");
-  return <div className="">Header</div>;
+  return <div className="text-3xl font-bold">Header</div>;
 };
 
 export default React.memo(Header);
